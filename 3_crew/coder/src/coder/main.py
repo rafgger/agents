ddac0001e9752fab9820b37b99924b73d1dee77c
Rafael <rafgger@gmail.com>
2026-04-04 20:27:48 +0200

    crewai coding

--- 3_crew/coder/src/coder/main.py
+++ 3_crew/coder/src/coder/main.py
@@ -4,7 +4,7 @@ import warnings
 import os
 from datetime import datetime
 
-from coder.crew import Coder
+from coder.crew import Coder # docker needs to be running
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -14,7 +14,7 @@ os.makedirs('output', exist_ok=True)
 assignment = 'Write a python program to calculate the first 10,000 terms \
     of this series, multiplying the total by 4: 1 - 1/3 + 1/5 - 1/7 + ...'
 
-def run():
+def run(): # cd 3_crew/coder; crewai run
     """
     Run the crew.
     """
